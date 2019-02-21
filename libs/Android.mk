aee26c87ae9b23a3d079c80654309058b479b7ff
deqin.tang <deqin.tang@tcl.com>
2019-02-21 17:11:06 +0800

    rm_CB

--- libs/Android.mk
+++
@@ -1,7 +0,0 @@
-include $(CLEAR_VARS)
-
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := tct-qct-mms-ext:libs/tct-qct-mms-encapsulation.jack
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += tct-mtk-mms-ext:libs/tct-mtk-mms-encapsulation.jar
-
-include $(BUILD_MULTI_PREBUILT)
-
--- tct-wrapper-src/com/tct/wrapper/WrapConstants.java
+++
@@ -1,6 +0,0 @@
-package com.tct.wrapper;
-
-
-class WrapConstants {
-    public static final String TAG = "wrap-cb";
-}
